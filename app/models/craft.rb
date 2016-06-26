36e200d5f3dfc5e2c16ccd7bb25ee6f57a02b4bf
bestar-zhao <tlnzwcc@gmail.com>
2016-06-26 20:05:48 +0800

    忘记删除craft.rb中的after_action

--- app/models/craft.rb
+++ app/models/craft.rb
@@ -1,11 +1,3 @@
 class Craft < ActiveRecord::Base
   belongs_to :order
-
-  after_save :update_order_or_indent
-
-  # 修改子订单和总订单
-  def update_order_or_indent
-    # 调用 OrdersHelper 中的 update_order_and_indent 方法
-    update_order_and_indent(self.order)
-  end
 end
