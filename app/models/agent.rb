39f5297e425d941840ef2060e3c49ebad3d51e20
bestar-zhao <tlnzwcc@gmail.com>
2016-10-13 19:58:48 +0800

    修改代理商地址生成方式、子订单收货地址生成逻辑

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -7,12 +7,30 @@ class Agent < ActiveRecord::Base
   #validates :full_name, uniqueness: {scope: :name}
   #
 
-  def full_address
-    [ChinaCity.get(self.province), ChinaCity.get(self.city), ChinaCity.get(self.district), self.town, self.address].join('')
+  def delivery_address
+    if self.city.present?
+      tmp_address = ""
+      city_name = ChinaCity.get(city)
+      if city_name =~ /(自治州|单位)$/
+        if self.district.present?
+          tmp_address = ChinaCity.get(district)
+        else
+          tmp_address = city_name
+        end
+      elsif city_name =~ /(市辖区|县)$/
+        tmp_address = ChinaCity.get(province)
+      end
+      tmp_address.gsub("地区", "")
+    else
+      tmp_address = ChinaCity.get(province)
+    end
+    binding.pry
+    tmp_address
+    # [ChinaCity.get(self.province), ChinaCity.get(self.city), ChinaCity.get(self.district), self.town, self.address].join('')
   end
 
   protected
   def generate_address
-    self.address = [ChinaCity.get(self.province), ChinaCity.get(self.city), ChinaCity.get(self.district)].join("")
+    self.address = [ChinaCity.get(self.province), ChinaCity.get(self.city), ChinaCity.get(self.district)].join("") if self.address.blank?
   end
 end
