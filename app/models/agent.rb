f54699244d03bd977fc5d7aaf011a8f1883d3f07
bestar-zhao <tlnzwcc@gmail.com>
2016-10-26 17:53:44 +0800

    找回Agent修改

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -32,29 +32,44 @@ class Agent < ActiveRecord::Base
     #   tmp_address = ChinaCity.get(province)
     # end
     tmp_address = ""
+    province_name = Province.find(province).name.gsub("省", "")
     if self.city.present?
       city_name = City.find(city).name
       if (city_name =~ /(自治州|区划)$/).present?
-        tmp_address = Province.find(province).name.gsub("省", "") + city_name.gsub("土家族苗族自治州", "").gsub("省直辖县级行政区划", "")
+        tmp_address =  province_name + city_name.gsub("土家族苗族自治州", "").gsub("省直辖县级行政区划", "")
         if self.district.present?
-          tmp_address = city_name.gsub("土家族苗族自治州", "").gsub("省直辖县级行政区划", "") + District.find(district).name.gsub("市", "").gsub("县", "").gsub("林区", "")
+          tmp_address = tmp_address + District.find(district).name.gsub("市", "").gsub("县", "").gsub("林区", "")
+        elsif self.town.present?
+          tmp_address = tmp_address + town
+        else
+          tmp_address = tmp_address
         end
       elsif (city_name =~ /(市辖区|县)$/).present?
-        tmp_address = Province.find(province).name
+        tmp_address = province_name
         if self.district.present?
           tmp_district = District.find(district).name
           tmp_address = tmp_address + (tmp_district.length > 2 ? tmp_district.gsub("县", "") : tmp_district)
+        elsif self.town.present?
+          tmp_address = tmp_address + town
+        else
+          tmp_address = tmp_address
         end
       else
-        tmp_address = Province.find(province).name.gsub("省", "") + city_name
+        tmp_address = province_name + city_name.gsub("市", "")
         if self.district.present?
-          tmp_district =  District.find(district).name.gsub("市辖区", "").gsub("土家族自治县", "")
-          tmp_address = city_name + (tmp_district.length > 2 ? tmp_district.gsub("区", "").gsub("县", "") : tmp_district)
+          tmp_district =  District.find(district).name.gsub("土家族自治县", "").gsub("市辖区","")
+          tmp_address = tmp_address + (tmp_district.length > 2 ? tmp_district.gsub("区", "").gsub("县", "") : tmp_district)
+          tmp_address = (tmp_address.gsub(province_name, "") + self.town) if self.town.present?
+        elsif self.town.present?
+          tmp_address = tmp_address + town
+        else
+          tmp_address = tmp_address
         end
       end
-      tmp_address = tmp_address.gsub("市", "").gsub("地区", "")
+      # tmp_address = tmp_address.gsub("市", "").gsub("地区", "")
     else
-      tmp_address = Province.find(province).name
+      tmp_address = province_name
+      tmp_address = tmp_address + town if self.town.present?
     end
     self.delivery_address = tmp_address
   end
