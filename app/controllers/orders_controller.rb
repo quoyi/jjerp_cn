65cde80cc5fa8845cd2eccc8da6b747476038821
bestar-zhao <bestar1988@qq.com>
2017-01-09 21:10:12 +0800

    未发货页面过滤条件调整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -317,7 +317,7 @@ class OrdersController < ApplicationController
     # 已拆单，且未打包 的总订单
     indent_condition = {}
     # indent_condition[:status] = Indent.statuses[:offered]..Indent.statuses[:packaged]
-    indent_condition[:max_status] = Indent.statuses[:packaged]..Indent.statuses[:over]
+    indent_condition[:max_status] = Indent.statuses[:packaged]..Indent.statuses[:sent]
     # indent_condition[:status] = Indent.statuses[:packaged]
     indent_condition[:agent_id] = params[:agent_id] if params[:agent_id].present?
     @indents = Indent.where(indent_condition)
