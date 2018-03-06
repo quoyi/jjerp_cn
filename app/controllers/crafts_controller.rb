08e84b0942bc578cb45d1946d7f496b98149e45c
beitaz <beitaz@icloud.com>
2018-03-06 14:35:21 +0800

    删除断点

--- app/controllers/crafts_controller.rb
+++ app/controllers/crafts_controller.rb
@@ -16,7 +16,6 @@ class CraftsController < ApplicationController
       craft_category = CraftCategory.find_by(id: key)
       total_number = 0 
       value.each{|craft| total_number += craft.number}
-      debugger
       crafts_arr << {name: craft_category.full_name, number: total_number} if craft_category
     end
     @crafts_arr = crafts_arr.sort_by{|m| m[:number] }
