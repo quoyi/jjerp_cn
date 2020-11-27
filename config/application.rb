e90fb67be8d3f5ffb7db6baa402b298139ad4519
Quo yi <quoyi007@gmail.com>
2020-11-28 03:07:06 +0800

    升级 6.1 完成

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
-require File.expand_path('../boot', __FILE__)
+require_relative "boot"
 
-# require 'csv'
-require 'rails/all'
+require "csv"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,18 +9,19 @@ Bundler.require(*Rails.groups)
 
 module Jjerp
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     config.time_zone = 'Beijing'
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
     config.i18n.default_locale = "zh-CN".to_sym
 
     config.autoload_paths << Rails.root.join("lib")
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
 
     config.generators do |g|
       g.helper false
