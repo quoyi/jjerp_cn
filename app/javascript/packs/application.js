1823ce3a41df36526bfbd248264512d48016d299
Quo yi <quoyi007@gmail.com>
2020-11-29 07:18:51 +0800

    添加自定义 btn 样式

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import '../stylesheets/application';
 
 import Rails from "@rails/ujs"
@@ -14,17 +9,15 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "bootstrap";
+import "bootstrap/dist/js/bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
 import "sticky-kit/dist/sticky-kit";
+import "owl.carousel/dist/owl.carousel";
 import "./theme";
 
-// Uncomment to copy all static images under ../img to the output folder and reference
+// Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-const images = require.context('../img', true)
-const imagePath = (name) => images(name, true)
-
-$(function() {
-  console.log('jQuery!');
-});
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+require.context('../images', true)
